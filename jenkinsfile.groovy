db6ca20849c727abd4742af25605d456136730e7
DevSeoRex <ckdekrn88@gmail.com>
2023-12-03 22:07:22 +0900

    ⚙️ chore: jenkinsfile Harbor IP 수정

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,7 +19,7 @@ node {
     }
 
     stage('Push image') {
-        docker.withRegistry('https://192.168.45.205', 'harbor') {
+        docker.withRegistry('http://211.205.161.133:5000', 'harbor') {
             app.push("latest")
         }
     }
